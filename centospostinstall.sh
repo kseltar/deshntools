6511a5f77b907d8175e92455f0da7130daa716a0
KSEltar <kseltar@gmail.com>
2013-02-13 18:40:37 -0200

    Update centospostinstall.sh
    more changes

--- centospostinstall.sh
+++ centospostinstall.sh
@@ -9,7 +9,7 @@
 #               network conf, security conf, dhcp conf, pxe, etc
 # Author:       @kseltar
 # DateInit:     23 Jan 2012
-# DateUpdate:   02 Feb 2013
+# DateUpdate:   13 Feb 2013
 # Version:      0.6
 
 #!/bin/bash
@@ -214,6 +214,10 @@ function f_esentialpaks () {
   yum -y groupinstall "Development Tools"
 }
 
+function f_desktop () {
+  yum groupinstall "Desktop" "Desktop Platform" "X Window System" "Fonts"
+}
+
 #function hhh () {
   while [ "$seleccion" != "exit" ]; do
   #  if [ "$seleccion" == "" ]; then
@@ -228,6 +232,8 @@ function f_esentialpaks () {
       "3" | "red" | "nertwork") f_ethconf; ;;
       "4" | "update" | "upgrade" | "actualizar") f_yumgrade; ;;
       "5" | "installplus" | "instalar") f_installplus; ;;
+      "6" | "isnstallplus" | "isnstalar") f_desktop; ;;
+      "7" | "sinstallplus" | "insstalar") f_esentialpaks; ;;
       "97" | "atrpms") f_atrpmrepo; ;;
       "98" | "reboot" | "reiniciar") f_reboot; ;;
       "99" | "salir" | "exit" | "quit") seleccion="exit"; ;;
@@ -244,6 +250,452 @@ function dasd () {
 #tar xvf $u/y.tar
 #cd $u
 #}
+
+Desktop
+Minimal Desktop
+Minimal
+Basic Server
+Database Server
+Web Server
+Virual Host
+Software Development Workstation
+
+-----
+function f_yumgrp01applications () {
+  yum -y groupinstall "Applications";
+}
+
+  yum -y groupinstall "Applications"
+
+
+  yum -y groupinstall "Emacs"
+  yum -y groupinstall "Graphics Creation Tools"
+  yum -y groupinstall "Internet Applications"
+  yum -y groupinstall "Internet Browser"
+  yum -y groupinstall "Office Suite and Productivity"
+  yum -y groupinstall "TeX Support"
+  yum -y groupinstall "Technical Writing"
+-----
+
+  yum -y groupinstall "Base System"
+
+  yum -y groupinstall "Backup Client"
+  yum -y groupinstall "Base"
+  yum -y groupinstall "Client management tools"
+  yum -y groupinstall "Compatibility libraries"
+  yum -y groupinstall "Console Internet tools"
+  yum -y groupinstall "Debugging tools"
+  yum -y groupinstall "Dial-up Networking Support"
+  yum -y groupinstall "Directory Client"
+  yum -y groupinstall "FCoE Storage Client"
+  yum -y groupinstall "Hardware monitoring utilities"
+  yum -y groupinstall "Infiniband Support"
+  yum -y groupinstall "Java Platform"
+  yum -y groupinstall "Large Systems Performance"
+  yum -y groupinstall "Legacy UNIX compatibility"
+  yum -y groupinstall "Mainframe Access"
+  yum -y groupinstall "Network file system client"
+  yum -y groupinstall "Networking Tools"
+  yum -y groupinstall "Performance Tools"
+  yum -y groupinstall "Perl Support"
+  yum -y groupinstall "Printing client"
+  yum -y groupinstall "Ruby Support"
+  yum -y groupinstall "Scientific support"
+  yum -y groupinstall "Security Tools"
+  yum -y groupinstall "Smart card support"
+  yum -y groupinstall "Storage Availability Tools"
+  yum -y groupinstall "iSCSI Storage Client"
+
+
+-----
+
+  yum -y groupinstall "Databases"
+
+  yum -y groupinstall "MySQL Database client"
+  yum -y groupinstall "MySQL Database server"
+  yum -y groupinstall "PostgreSQL Database client"
+  yum -y groupinstall "PostgreSQL Database server"
+
+
+-----
+
+  yum -y groupinstall "Desktops"
+
+  yum -y groupinstall "Desktop"
+  yum -y groupinstall "Desktop Debugging and Performance Tools"
+  yum -y groupinstall "Desktop Platform"
+  yum -y groupinstall "Fonts"
+  yum -y groupinstall "General Purpose Desktop"
+  yum -y groupinstall "Graphical Administration Tools"
+  yum -y groupinstall "Input Methods"
+  yum -y groupinstall "KDE Desktop"
+  yum -y groupinstall "Legacy X Window System compatibility"
+  yum -y groupinstall "Remote Desktop Clients"
+  yum -y groupinstall "X Window System"
+
+
+-----
+
+  yum -y groupinstall "Development"
+
+  yum -y groupinstall "Additional Development"
+  yum -y groupinstall "Desktop Platform Development"
+  yum -y groupinstall "Development tools"
+  yum -y groupinstall "Eclipse"
+  yum -y groupinstall "Server Platform Development"
+
+
+-----
+
+  yum -y groupinstall "High Availability"
+
+  yum -y groupinstall "High Availability"
+  yum -y groupinstall "High Availability Management"
+
+
+-----
+
+  yum -y groupinstall "Languages"
+
+  yum -y groupinstall "Afrikaans Support"
+  yum -y groupinstall "Albanian Support"
+  yum -y groupinstall "Arabic Support"
+  yum -y groupinstall "Armenian Support"
+  yum -y groupinstall "Assamese Support"
+  yum -y groupinstall "Azerbaijani Support"
+  yum -y groupinstall "Basque Support"
+  yum -y groupinstall "Belarusian Support"
+  yum -y groupinstall "Bengali Support"
+  yum -y groupinstall "Bhutanese Support"
+  yum -y groupinstall "Brazilian Portuguese Support"
+  yum -y groupinstall "Breton Support"
+  yum -y groupinstall "Bulgarian Support"
+  yum -y groupinstall "Catalan Support"
+  yum -y groupinstall "Chhattisgarhi Support"
+  yum -y groupinstall "Chichewa Support"
+  yum -y groupinstall "Chinese Support"
+  yum -y groupinstall "Coptic Support"
+  yum -y groupinstall "Croatian Support"
+  yum -y groupinstall "Czech Support"
+  yum -y groupinstall "Danish Support"
+  yum -y groupinstall "English (UK) Support"
+  yum -y groupinstall "Esperanto Support"
+  yum -y groupinstall "Ethiopic Support"
+  yum -y groupinstall "Faroese Support"
+  yum -y groupinstall "Fijian Support"
+  yum -y groupinstall "Filipino Support"
+  yum -y groupinstall "Finnish Support"
+  yum -y groupinstall "French Support"
+  yum -y groupinstall "Frisian Support"
+  yum -y groupinstall "Friulian Support"
+  yum -y groupinstall "Gaelic Support"
+  yum -y groupinstall "Galician Support"
+  yum -y groupinstall "Georgian Support"
+  yum -y groupinstall "German Support"
+  yum -y groupinstall "Greek Support"
+  yum -y groupinstall "Gujarati Support"
+  yum -y groupinstall "Hebrew Support"
+  yum -y groupinstall "Hiligaynon Support"
+  yum -y groupinstall "Hindi Support"
+  yum -y groupinstall "Hungarian Support"
+  yum -y groupinstall "Icelandic Support"
+  yum -y groupinstall "Indonesian Support"
+  yum -y groupinstall "Inuktitut Support"
+  yum -y groupinstall "Irish Support"
+  yum -y groupinstall "Italian Support"
+  yum -y groupinstall "Japanese Support"
+  yum -y groupinstall "Kannada Support"
+  yum -y groupinstall "Kashmiri Support"
+  yum -y groupinstall "Kashubian Support"
+  yum -y groupinstall "Kazakh Support"
+  yum -y groupinstall "Khmer Support"
+  yum -y groupinstall "Kinyarwanda Support"
+  yum -y groupinstall "Konkani Support"
+  yum -y groupinstall "Korean Support"
+  yum -y groupinstall "Kurdish Support"
+  yum -y groupinstall "Lao Support"
+  yum -y groupinstall "Latin Support"
+  yum -y groupinstall "Latvian Support"
+  yum -y groupinstall "Lithuanian Support"
+  yum -y groupinstall "Low Saxon Support"
+  yum -y groupinstall "Luxembourgish Support"
+  yum -y groupinstall "Macedonian Support"
+  yum -y groupinstall "Maithili Support"
+  yum -y groupinstall "Malagasy Support"
+  yum -y groupinstall "Malay Support"
+  yum -y groupinstall "Malayalam Support"
+  yum -y groupinstall "Maltese Support"
+  yum -y groupinstall "Manx Support"
+  yum -y groupinstall "Maori Support"
+  yum -y groupinstall "Marathi Support"
+  yum -y groupinstall "Mongolian Support"
+  yum -y groupinstall "Myanmar (Burmese) Support"
+  yum -y groupinstall "Nepali Support"
+  yum -y groupinstall "Northern Sotho Support"
+  yum -y groupinstall "Norwegian Support"
+  yum -y groupinstall "Occitan Support"
+  yum -y groupinstall "Oriya Support"
+  yum -y groupinstall "Persian Support"
+  yum -y groupinstall "Polish Support"
+  yum -y groupinstall "Portuguese Support"
+  yum -y groupinstall "Punjabi Support"
+  yum -y groupinstall "Romanian Support"
+  yum -y groupinstall "Russian Support"
+  yum -y groupinstall "Sanskrit Support"
+  yum -y groupinstall "Sardinian Support"
+  yum -y groupinstall "Serbian Support"
+  yum -y groupinstall "Sindhi Support"
+  yum -y groupinstall "Sinhala Support"
+  yum -y groupinstall "Slovak Support"
+  yum -y groupinstall "Slovenian Support"
+  yum -y groupinstall "Somali Support"
+  yum -y groupinstall "Southern Ndebele Support"
+  yum -y groupinstall "Southern Sotho Support"
+  yum -y groupinstall "Spanish Support"
+  yum -y groupinstall "Swahili Support"
+  yum -y groupinstall "Swati Support"
+  yum -y groupinstall "Swedish Support"
+  yum -y groupinstall "Tagalog Support"
+  yum -y groupinstall "Tajik Support"
+  yum -y groupinstall "Tamil Support"
+  yum -y groupinstall "Telugu Support"
+  yum -y groupinstall "Tetum Support"
+  yum -y groupinstall "Thai Support"
+  yum -y groupinstall "Tibetan Support"
+  yum -y groupinstall "Tsonga Support"
+  yum -y groupinstall "Tswana Support"
+  yum -y groupinstall "Turkish Support"
+  yum -y groupinstall "Turkmen Support"
+  yum -y groupinstall "Ukrainian Support"
+  yum -y groupinstall "Upper Sorbian Support"
+  yum -y groupinstall "Urdu Support"
+  yum -y groupinstall "Uzbek Support"
+  yum -y groupinstall "Venda Support"
+  yum -y groupinstall "Vietnamese Support"
+  yum -y groupinstall "Walloon Support"
+  yum -y groupinstall "Welsh Support"
+  yum -y groupinstall "Xhosa Support"
+  yum -y groupinstall "Zulu Support"
+
+
+-----
+
+  yum -y groupinstall "Load Balancer"
+
+  yum -y groupinstall "Load Balancer"
+
+-----
+
+  yum -y groupinstall "Resilient Storage"
+
+  yum -y groupinstall "Resilient Storage"
+
+-----
+
+  yum -y groupinstall "Scalable Filesystem Support"
+
+  yum -y groupinstall "Scalable Filesystem"
+
+
+-----
+
+  yum -y groupinstall "Servers"
+
+  yum -y groupinstall "Backup Server"
+  yum -y groupinstall "CIFS file server"
+  yum -y groupinstall "Directory Server"
+  yum -y groupinstall "E-mail server"
+  yum -y groupinstall "FTP Server"
+  yum -y groupinstall "Identity Management Server"
+  yum -y groupinstall "NFS file server"
+  yum -y groupinstall "Network Infrastructure Server"
+  yum -y groupinstall "Network Storage Server"
+  yum -y groupinstall "Print Server"
+  yum -y groupinstall "Server Platform"
+  yum -y groupinstall "System administration tools"
+
+
+-----
+
+  yum -y groupinstall "System Management"
+
+  yum -y groupinstall "Messaging Client Support"
+  yum -y groupinstall "SNMP Support"
+  yum -y groupinstall "System Management"
+  yum -y groupinstall "System Management and Messaging Server support"
+  yum -y groupinstall "Web-Based Enterprise Management"
+
+-----
+
+  yum -y groupinstall "Virtualization"
+
+  yum -y groupinstall "Virtualization"
+  yum -y groupinstall "Virtualization Client"
+  yum -y groupinstall "Virtualization Platform"
+  yum -y groupinstall "Virtualization Tools"
+
+-----
+
+  yum -y groupinstall "Web Services"
+
+  yum -y groupinstall "PHP Support"
+  yum -y groupinstall "TurboGears application framework"
+  yum -y groupinstall "Web Server"
+  yum -y groupinstall "Web Servlet Engine"
+
+   Arabic Support [ar]
+   Armenian Support [hy]
+   Assamese Support [as]
+   Bengali Support [bn]
+   Bhutanese Support [dz]
+   Chinese Support [zh]
+   Ethiopic Support [am]
+   Georgian Support [ka]
+   Gujarati Support [gu]
+   Hebrew Support [he]
+   Hindi Support [hi]
+   Inuktitut Support [iu]
+   Japanese Support [ja]
+   Kannada Support [kn]
+   Khmer Support [km]
+   Konkani Support [kok]
+   Korean Support [ko]
+   Kurdish Support [ku]
+   Lao Support [lo]
+   Maithili Support [mai]
+   Malayalam Support [ml]
+   Marathi Support [mr]
+   Myanmar (Burmese) Support [my]
+   Oriya Support [or]
+   Punjabi Support [pa]
+   Sinhala Support [si]
+   Tajik Support [tg]
+   Tamil Support [ta]
+   Telugu Support [te]
+   Thai Support [th]
+   Urdu Support [ur]
+   Venda Support [ve]
+Available Groups:
+   Backup Client
+   Backup Server
+   Client management tools
+   Eclipse
+   Emacs
+   FCoE Storage Client
+   High Availability
+   High Availability Management
+   Identity Management Server
+   Infiniband Support
+   KDE Desktop
+   Load Balancer
+   Mainframe Access
+   Messaging Client Support
+   Remote Desktop Clients
+   Resilient Storage
+   Ruby Support
+   Scalable Filesystems
+   Server Platform Development
+   Smart card support
+   Somali Support
+   System Management
+   Systems Management Messaging Server support
+   TeX support
+   Technical Writing
+   TurboGears application framework
+   Virtualization Platform
+   Web Servlet Engine
+   Web-Based Enterprise Management
+Available Language Groups:
+   Afrikaans Support [af]
+   Albanian Support [sq]
+   Amazigh Support [ber]
+   Azerbaijani Support [az]
+   Basque Support [eu]
+   Belarusian Support [be]
+   Brazilian Portuguese Support [pt_BR]
+   Breton Support [br]
+   Bulgarian Support [bg]
+   Catalan Support [ca]
+   Chhattisgarhi Support [hne]
+   Chichewa Support [ny]
+   Coptic Support [cop]
+   Croatian Support [hr]
+   Czech Support [cs]
+   Danish Support [da]
+   Dutch Support [nl]
+   English (UK) Support [en_GB]
+   Esperanto Support [eo]
+   Estonian Support [et]
+   Faroese Support [fo]
+   Fijian Support [fj]
+   Filipino Support [fil]
+   Finnish Support [fi]
+   French Support [fr]
+   Frisian Support [fy]
+   Friulian Support [fur]
+   Gaelic Support [gd]
+   Galician Support [gl]
+   German Support [de]
+   Greek Support [el]
+   Hiligaynon Support [hil]
+   Hungarian Support [hu]
+   Icelandic Support [is]
+   Indonesian Support [id]
+   Interlingua Support [ia]
+   Irish Support [ga]
+   Italian Support [it]
+   Kashmiri Support [ks]
+   Kashubian Support [csb]
+   Kazakh Support [kk]
+   Kinyarwanda Support [rw]
+   Latin Support [la]
+   Latvian Support [lv]
+   Lithuanian Support [lt]
+   Low Saxon Support [nds]
+   Luxembourgish Support [lb]
+   Macedonian Support [mk]
+   Malagasy Support [mg]
+   Malay Support [ms]
+   Maltese Support [mt]
+   Manx Support [gv]
+   Maori Support [mi]
+   Mongolian Support [mn]
+   Nepali Support [ne]
+   Northern Sotho Support [nso]
+   Norwegian Support [nb]
+   Occitan Support [oc]
+   Persian Support [fa]
+   Polish Support [pl]
+   Portuguese Support [pt]
+   Romanian Support [ro]
+   Russian Support [ru]
+   Sanskrit Support [sa]
+   Sardinian Support [sc]
+   Serbian Support [sr]
+   Sindhi Support [sd]
+   Slovak Support [sk]
+   Slovenian Support [sl]
+   Southern Ndebele Support [nr]
+   Southern Sotho Support [st]
+   Spanish Support [es]
+   Swahili Support [sw]
+   Swati Support [ss]
+   Swedish Support [sv]
+   Tagalog Support [tl]
+   Tetum Support [tet]
+   Tibetan Support [bo]
+   Tsonga Support [ts]
+   Tswana Support [tn]
+   Turkish Support [tr]
+   Turkmen Support [tk]
+   Ukrainian Support [uk]
+   Upper Sorbian Support [hsb]
+   Uzbek Support [uz]
+   Vietnamese Support [vi]
+   Walloon Support [wa]
+   Welsh Support [cy]
+   Xhosa Support [xh]
+   Zulu Support [zu]
 
 
 yum -y update
